f2b0115567a31ee191d03a4a5274f84772e283ee
Gilian Ponte <gilianponte@live.nl>
2018-08-30 16:12:27 +0200

    Update HowtodealwithJavaScript.R

--- 02_ScrapingwithJavaScript/HowtodealwithJavaScript.R
+++ 02_ScrapingwithJavaScript/HowtodealwithJavaScript.R
@@ -2,9 +2,17 @@ library(rvest)
 
 rm(list = ls())
 
+## download phantomjs here: http://phantomjs.org/download.html
+## get the working directory you are working in
+getwd()
+
+## store the phantom.exe file in your working directory
+
+## Start scraping
 url <- "https://www.ah.nl/producten/product/wi200726/ah-aansteker-flex"
 print(url)
 
+## Simulate a browser and run JavaScript
 writeLines(sprintf(paste0("var url ='",url,"';
                           var page = new WebPage()
                           var fs = require('fs');
@@ -22,15 +30,17 @@ writeLines(sprintf(paste0("var url ='",url,"';
                           }"), url), con="ah_pricing.js")
 
 
+## Run the defined JavaScript in the browser and write a html file.
 system("phantomjs ah_pricing.js > ah_pricing.html")
 write(readLines(pipe("phantomjs ah_pricing.js", "r")), "ah_pricing.html")
 
+## Load the html file and scrape
 html <- "ah_pricing.html"
 pg <- read_html(html)
 
 # Exercise 1: Try to scrape the price of the lighter now
 price <- pg %>% 
-  html_node(..) %>% 
+  html_node("") %>% 
   html_text()
 
 print(price)
