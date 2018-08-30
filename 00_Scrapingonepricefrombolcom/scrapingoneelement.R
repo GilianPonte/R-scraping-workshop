983207e0bffc2c61aad95a54d21d624622623496
Gilian Ponte <gilianponte@live.nl>
2018-08-30 19:59:35 +0200

    Update scrapingoneelement.R

--- 00_Scrapingonepricefrombolcom/scrapingoneelement.R
+++ 00_Scrapingonepricefrombolcom/scrapingoneelement.R
@@ -25,7 +25,7 @@ author <- read_html("") %>%
 
 print(author)
 
-# Exercise 2: This is a product from Amazon.de. Try to scrape the price. You can use the code from excrsice 1.
+# Exercise 2: This is a product from Amazon.de. Try to scrape the price. You can use the code from exercise 1.
 url <- "https://www.amazon.de/LG-Products-29UM68-P-DisplayPort-Reaktionszeit/dp/B01AWG59V6?pd_rd_wg=vt28V&pd_rd_r=8e2846d5-6ac0-4c5e-9754-6ed132b0dc6b&pd_rd_w=H8MSq&ref_=pd_gw_simh&pf_rd_r=ZM8RNJDM5HRJA15SPKA8&pf_rd_p=c4fdae14-5430-5b71-9fd2-0dd831fbb997"
 
 
